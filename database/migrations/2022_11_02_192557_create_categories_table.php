aac2af6a611eedbb2030e8bbfadda1e3b173d20c
Hani <h.gerges@vardot.com>
2022-11-04 16:42:48 +0300

    Add tables migration files

--- database/migrations/2022_11_02_192557_create_categories_table.php
+++ database/migrations/2022_11_02_192557_create_categories_table.php
@@ -13,16 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['income', 'expenses']);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_number')->unique();
-            $table->string('birth_date');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user']);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
